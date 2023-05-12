58317546af2b5c668fda6d330d773414c2b9d4ee
Emanuel Zabaleta <69875057+emanuelzm@users.noreply.github.com>
2023-05-12 10:30:54 -0500

    g.3

--- public/assets/js/newsletter.js
+++ public/assets/js/newsletter.js
@@ -1,22 +1,15 @@
-var formContactV1 = document.getElementById("formContactV1");
-formContactV1.addEventListener("submit", formSubmit);
-var code = atob("ODc5ZWVlNDUtMGY2My00MzFjLTkxMjEtNTFjYjA0ZTFkYmY2");
+var formV1 = document.getElementById("formV1");
+formV1.addEventListener("submit", formSubmit);
+var code = atob("MDYwZWI2OGQtZTZkYi00NDRiLWE2MmUtOTRiYzRlMWE3YjE4");
 var fetch_code = atob("aHR0cHM6Ly9nZXRmb3JtLmlvL2Yv") + code;
+
 function formSubmit(e) {
     e.preventDefault();
 
     const formData = new FormData();
     formData.append(
-        'Nombre',
-        document.querySelector('input[name="form_name"]').value
-    )
-    formData.append(
         'Correo',
         document.querySelector('input[name="form_email"]').value
-    )
-    formData.append(
-        'Mensaje',
-        document.querySelector('textarea[name="form_message"]').value
     )
 
     fetch(fetch_code, {
